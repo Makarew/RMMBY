8239bc201cd56cf445cc6786a1b6bc1f2452394b
Makarew <makarew@gmail.com>
2022-11-24 15:23:28 -0700

    Add project files.

---
+++ RMMBY/Core/LoadModMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+using MelonLoader;
+
+namespace RMMBY
+{
+    internal class LoadModMenu
+    {
+        private static AssetBundle bundle;
+        public static string scene;
+
+        private static void GetModMenu()
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+        public static void CheckForBundle()
+        {
+            if (bundle == null)
+            {
+                string path = Path.Combine(MelonHandler.ModsDirectory, "RMMBY/rmmby");
+                bundle = AssetBundle.LoadFromFile(path);
+                string[] scenePaths = bundle.GetAllScenePaths();
+                scene = Path.GetFileNameWithoutExtension(scenePaths[0]);
+            }
+
+            GetModMenu();
+        }
+    }
+}
---
+++ RMMBY/Core/ModToggleButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace RMMBY
+{
+    internal class ModToggleButton : MonoBehaviour
+    {
+        private ModMenuHandler sys;
+
+        private bool isHighlighted;
+        private GameObject highlight;
+
+        private void Start()
+        {
+            sys = GameObject.FindObjectOfType<ModMenuHandler>();
+            highlight = transform.Find("Highlight").gameObject;
+        }
+
+        private void Update()
+        {
+            if (!isHighlighted && sys.selectedObject == gameObject)
+            {
+                isHighlighted = true;
+                highlight.SetActive(true);
+            } else if (isHighlighted && sys.selectedObject != gameObject)
+            {
+                highlight.SetActive(false);
+                isHighlighted = false;
+            }
+        }
+    }
+}
